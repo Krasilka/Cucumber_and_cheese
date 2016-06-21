cfafe5b070b5ac0ad571543d31271a42ca046325
Iryna Krasylnykova <iryna.krasylnykova@lazada.com>
2016-06-21 18:55:40 +0700

    modified_page_101

--- Watir_project/features/adopting_puppies.feature
+++ Watir_project/features/adopting_puppies.feature
@@ -8,7 +8,7 @@ Feature: Adopting puppies
     Given I am on the puppy adoption site
 
   Scenario Outline: Adopting one puppy
-    When I click the first View Details button
+    When I click the View Details button for "Brook"
     When I click the Adopt Me button
     And I click the Complete the Adoption button
     And I enter "<name>" in the name field
@@ -24,37 +24,52 @@ Feature: Adopting puppies
     |Joseph|555 South 32   |joe@hee.com       |Check         |
     |James |87456 skudfh   |janme@jdh.com     |Purchase order|
 
-
   Scenario: Adopting two puppies
-    When I click the first View Details button
+    When I click the View Details button for "Brook"
     And I click the Adopt Me button
     And I click Adopt Another Puppy button
-    And I click the second View Details button
+    And I click the View Details button for "Hanna"
     And I click the Adopt Me button
     And I click the Complete the Adoption button
-    And I enter "Cheezy" in the name field
-    And I enter "123 Main Street" in the address field
-    And I enter "cheezy@example.com" in the email field
-    And I select "Credit card" from the pay with dropdown
-    And I click the Place Order button
+    And I complete the adoption with:
+      |name  |address        |email             |pay_type      |
+      |Cheezy|123 Main Street|cheezy@example.com|Credit card   |
+    Then I should see "Thank you for adopting a puppy!"
+
+  Scenario: Adopting two puppies
+    When I click the View Details button for "Brook"
+    And I click the Adopt Me button
+    And I click the Complete the Adoption button
+    And I complete the adoption
+    Then I should see "Thank you for adopting a puppy!"
+
+  Scenario: Adopting one puppy
+    When I click the View Details button for "Brook"
+    When I click the Adopt Me button
+    And I click the Complete the Adoption button
+    When I complete the adoption using a Credit card
     Then I should see "Thank you for adopting a puppy!"
 
   Scenario: Validating cart with one puppy
-    When I click the first View Details button
+    When I click the View Details button for "Brook"
     When I click the Adopt Me button
     Then I should see "Brook" as the name for line item 1
     And I should see "$34.95" as the subtotal for line item 1
     And I should see "$34.95" as the cart total
 
-  @test
   Scenario: Validating cart with two puppies
-    When I click the first View Details button
+    When I click the View Details button for "Brook"
     When I click the Adopt Me button
     And I click Adopt Another Puppy button
-    And I click the second View Details button
+    And I click the View Details button for "Hanna"
     And I click the Adopt Me button
     Then I should see "Brook" as the name for line item 1
     And I should see "$34.95" as the subtotal for line item 1
     And I should see "Hanna" as the name for line item 2
     And I should see "$22.99" as the subtotal for line item 2
     And I should see "$57.94" as the cart total
+
+    @test
+  Scenario: Name is a required field
+    When I checkout leaving the name field blank
+    Then I should see the error message "Name can't be blank"
--- Watir_project/features/step_definitions/adopting_puppies_steps.rb
+++ Watir_project/features/step_definitions/adopting_puppies_steps.rb
@@ -1,64 +1,86 @@
 Given(/^I am on the puppy adoption site$/) do
-  @browser.goto "http://puppies.herokuapp.com"
+  visit(HomePage)
 end
 
-When(/^I click the first View Details button$/) do
-  @browser.button(:value => "View Details", :index => 0).click
-end
-
-When(/^I click the second View Details button$/) do
-  @browser.button(:value => "View Details", :index => 1).click
+When(/^I click the View Details button for "(.*?)"$/) do |name|
+  on(HomePage).select_puppy name
 end
 
 And(/^I click the Adopt Me button$/) do
-  @browser.button(:value => "Adopt Me!").click
+  on(DetailsPage).add_to_cart
 end
 
 And(/^I click the Complete the Adoption button$/) do
-  @browser.button(:value => "Complete the Adoption").click
+  on(ShoppingCartPage).proceed_to_checkout
 end
 
-And(/^I enter "(.*?)" in the name field$/) do |arg1|
-  @browser.text_field(:id => "order_name").set(arg1)
+And(/^I enter "(.*?)" in the name field$/) do |name|
+  on(CheckoutPage).name = name
 end
 
-And(/^I enter "(.*?)" in the address field$/) do |arg1|
-  @browser.text_field(:id => "order_address").set(arg1)
+And(/^I enter "(.*?)" in the address field$/) do |address|
+  on(CheckoutPage).address = address
 end
 
-And(/^I enter "(.*?)" in the email field$/) do |arg1|
-  @browser.text_field(:id => "order_email").set(arg1)
+And(/^I enter "(.*?)" in the email field$/) do |email|
+  on(CheckoutPage).email = email
 end
 
-And(/^I select "(.*?)" from the pay with dropdown$/) do |arg1|
-  @browser.select_list(:id => "order_pay_type").select(arg1)
+And(/^I select "(.*?)" from the pay with dropdown$/) do |pay_type|
+  on(CheckoutPage).pay_type = pay_type
 end
 
 And(/^I click the Place Order button$/) do
-  @browser.button(:value => "Place Order").click
+  on(CheckoutPage).place_order
 end
 
 And(/^I click Adopt Another Puppy button$/) do
-  @browser.button(:value => "Adopt Another Puppy").click
+  on(ShoppingCartPage).continue_shopping
 end
 
 Then(/^I should see "(.*?)"$/) do |expected|
-  @browser.text.should include expected
+  @current_page.text.should include expected
 end
 
-Then(/^I should see "(.*?)" as the name for line item (\d+)$/) do |name, line_item|
-  row = (line_item.to_i-1)*6
-  @browser.table(:index => 0)[row][1].text.should include name
+Then(/^I should see "(.*?)" as the name for (line item \d+)$/) do |name, line_item|
+  on(ShoppingCartPage).name_for_line_item(line_item).should include name
 end
 
-And(/^I should see "(.*?)" as the subtotal for line item (\d+)$/) do |price, line_item|
-  row = (line_item.to_i-1)*6
-  @browser.table(:index => 0)[row][3].text.should == price
+And(/^I should see "(.*?)" as the subtotal for (line item \d+)$/) do |price, line_item|
+  on(ShoppingCartPage).subtotal_for_line_item(line_item).should == price
 end
 
 And(/^I should see "(.*?)" as the cart total$/) do |total_price|
-  @browser.td(:class => 'total_cell').text.should == total_price
+  on(ShoppingCartPage).cart_total.should == total_price
 end
 
+Then(/^I should see the error message "(.+)"$/) do |msg|
+  # @current_page.error_messages.should include msg
+  @current_page.should have_error_message msg
+end
 
+When(/^I complete the adoption with:$/) do |table|
+  on(CheckoutPage).checkout(table.hashes.first)
+end
 
+When(/^I complete the adoption$/) do
+  on(CheckoutPage).checkout
+end
+
+When(/^I complete the adoption using a Credit card$/) do
+  on(CheckoutPage).checkout('pay_type' => 'Credit card')
+end
+
+When /^I complete the adoption of a puppy$/ do
+  on(HomePage).select_puppy
+  on(DetailsPage).add_to_cart
+  on(ShoppingCartPage).proceed_to_checkout
+  on(CheckoutPage).checkout
+end
+
+When /^I checkout leaving the name field blank$/ do
+  on(HomePage).select_puppy
+  on(DetailsPage).add_to_cart
+  on(ShoppingCartPage).proceed_to_checkout
+  on(CheckoutPage).checkout('name' => '')
+end
